61f156ccabd225cbbcb81d45621f9c5a80975371
Sean Lynn <sean.lynn@hpe.com>
2020-06-16 18:14:52 -0500

    Add ability to load and check Kea leases.
    Updates to manager.py now allow it to query Kea and retrieve
    active leases.  The new test_lease_loader.py is a quick test
    harness that loads leases into Kea for testing with manager.py.

--- manager.py
+++ manager.py
@@ -1,11 +1,59 @@
 #!/usr/bin/env python3
 
+import sys
+import json
+import requests
+
+
+#
+# Query Kea for active server lease information
+#
 print('Querying Kea in the cluster to find any updated records we need to set')
-# TODO: build out records mgr here:
-#  1. Query Kea to get current state of reservations
-#  2. Get our cray-dns-unbound configmap storing our unbound A records JSON
-#  3. Determine if any diff exists between Kea's reservation set, and what we know about in the a records JSON
-#     a. If no diff, we can exit this job
-#     b. If there is a diff...
-# 4. modify our local A records JSON data set and kubectl patch the configmap with the updated set
-# 5. `kubectl rollout restart` our deployment so that the deployment pods/containers can begin to restart with the newer A records
+# TODO  - Kea API endpoint should come from K8S
+kea_api_endpoint = 'http://cray-dhcp-kea-api:8000'
+kea_headers = {"Content-Type": "application/json"}
+kea_request = {"command": "lease4-get-all", "service": [ "dhcp4" ] }
+
+try:
+    # Convert explicitly to json to catch errors up front.
+    kea_request_json = json.dumps(kea_request)
+
+    # Call Kea to retrieve active kea_response
+    kea_response = requests.post(url = kea_api_endpoint, \
+                           headers = kea_headers, \
+                           data = kea_request_json)
+    kea_response.raise_for_status()
+
+    kea_response_json = kea_response.json()
+except requests.exceptions.ConnectionError as err:
+    print('Cannot connect to URL: {}'.format(url))
+    raise SystemExit(err)
+except requests.exceptions.Timeout as err:
+    print('Timeout for request: {}'.format(url))
+    raise SystemExit(err)
+except requests.exceptions.RequestException as err:
+    print('Error occurred in kea response from: {}'.format(kea_response.url))
+    print('    HTTP error code: {}'.format(kea_response.status_code))
+    print('    HTTP response  : {}'.format(kea_response.text))
+    raise SystemExit(err)
+except Exception as err:
+    raise SystemExit(err)
+
+kea_response_json = kea_response.json()
+kea_return_code = kea_response_json[0]['result']
+
+if kea_return_code != 0:
+    print('Kea HTTP call success, but error in results:')
+    print('    Return code: {}'.format(kea_return_code))
+    print('    Data       : {}'.format(kea_response_json))
+    raise SystemExit()
+
+kea_leases = kea_response_json[0]['arguments']['leases']
+for lease in leases:
+    print(lease)
+
+
+#
+# Load and parse local DNS config file for current DNS entries
+#
+print('Loading live DNS entries from local configuration file')
