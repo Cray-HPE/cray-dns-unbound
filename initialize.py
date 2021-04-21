f0a112583c3a75bed9fde4138b9d42f751eff79b
Dat Le <dle@cray.com>
2021-04-21 11:12:47 -0500

    forgot f_in

--- initialize.py
+++ initialize.py
@@ -12,14 +12,15 @@ records_json_path = '{}/records.json.gz'.format(os.environ['UNBOUND_CONFIG_DIREC
 records_conf_path = '{}/records.conf'.format(os.environ['UNBOUND_CONFIG_DIRECTORY'])
 
 print('Reading A records JSON file at {} and translating to {}'.format(records_json_path, records_conf_path))
-with gzip.open(records_json_path) as fin:
-    shutil.copyfileobj(fin, fout)
-    records = json.loads(fout.read())
-    for zone in records:
-        records_conf = '{}local-data: "{} A {}"\nlocal-data-ptr: "{} {}"\n'.format(
-            records_conf, zone['hostname'], zone['ip-address'], zone['ip-address'], zone['hostname'])
-        records_conf = '{}local-data: "{}.local A {}"\nlocal-data-ptr: "{} {}.local"\n'.format(
-            records_conf, zone['hostname'], zone['ip-address'], zone['ip-address'], zone['hostname'])
+with open(records_json_path) as f_in:
+    with gzip.open(records_json_path) as f_out:
+        shutil.copyfileobj(f_in, f_out)
+        records = json.loads(f_out.read())
+        for zone in records:
+            records_conf = '{}local-data: "{} A {}"\nlocal-data-ptr: "{} {}"\n'.format(
+                records_conf, zone['hostname'], zone['ip-address'], zone['ip-address'], zone['hostname'])
+            records_conf = '{}local-data: "{}.local A {}"\nlocal-data-ptr: "{} {}.local"\n'.format(
+                records_conf, zone['hostname'], zone['ip-address'], zone['ip-address'], zone['hostname'])
 
 with open(records_conf_path, 'w') as f:
     f.write(records_conf)
