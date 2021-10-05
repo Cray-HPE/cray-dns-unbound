160fe50809fe8af1a517921e2567dea30a9d74de
Dat Le <dle@cray.com>
2021-10-04 22:52:36 -0500

    increase wait on startup

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$@" == "" ]]; then
-  sleep 5
+  sleep 10
   /srv/unbound/initialize.py
   unbound -c ${UNBOUND_CONFIG_DIRECTORY}/unbound.conf &
   while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done
