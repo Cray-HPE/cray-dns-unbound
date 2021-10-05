7c935ed8393ddc7e0c81acdef34b14459ae0aac9
Dat Le <dle@cray.com>
2021-10-04 23:10:52 -0500

    add while statement to pid check for race condition

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 if [[ "$@" == "" ]]; then
   /srv/unbound/initialize.py
   unbound -c ${UNBOUND_CONFIG_DIRECTORY}/unbound.conf &
-  sleep 5 &
   while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done
 else
   $@
