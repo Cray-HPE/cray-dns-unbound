d75179f6aa8b49a4dbf8361c0fac62cc541f3510
Dat Le <dle@cray.com>
2021-10-04 22:44:48 -0500

    adjust entrypoint to slowdown intial startup

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [[ "$@" == "" ]]; then
+  sleep 5
   /srv/unbound/initialize.py
   unbound -c ${UNBOUND_CONFIG_DIRECTORY}/unbound.conf &
   while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done
