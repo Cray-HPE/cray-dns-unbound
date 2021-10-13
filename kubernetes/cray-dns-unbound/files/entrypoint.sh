98dbbf6ec9023214b10518d6bdeafc6c3b1af405
Dat Le <dle@cray.com>
2021-10-12 20:10:00 -0500

    re-enable unbound-telemtry in entrypoint

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -5,8 +5,8 @@ set -ex
 if [[ "$@" == "" ]]; then
   /srv/unbound/initialize.py
   unbound -c ${UNBOUND_CONFIG_DIRECTORY}/unbound.conf &
-  while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done
-  #unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
+  while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
+  unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
 else
   $@
 fi
