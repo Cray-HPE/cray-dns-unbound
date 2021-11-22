65873f38a0e812b2d6211e8dc69b62b3539bb975
Dat Le <dle@cray.com>
2021-11-22 09:08:54 -0600

    add logic to restart unbound if pid is missing

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
+sleep 5
+
 if [[ "$@" == "" ]]; then
-  /srv/unbound/initialize.py
-  unbound -c ${UNBOUND_CONFIG_DIRECTORY}/unbound.conf &
   while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
   unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
 else
