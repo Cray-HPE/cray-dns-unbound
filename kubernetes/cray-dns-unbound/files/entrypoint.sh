defe4fd35fad30a1345c4c5398858038c38edd52
Dat Le <dle@cray.com>
2021-11-22 10:14:43 -0600

    adjust timing and update entrypoint

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -2,11 +2,8 @@
 
 set -ex
 
-sleep 5
+sleep 30
 
-if [[ "$@" == "" ]]; then
-  while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
-  unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
-else
-  $@
-fi
+while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
+unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
+
