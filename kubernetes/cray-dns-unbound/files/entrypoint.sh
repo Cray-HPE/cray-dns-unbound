279ff39d157f76c19ca54a4786f574d741bb0d30
Dat Le <dle@cray.com>
2021-11-22 22:47:08 -0600

    update entrypoint

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -4,6 +4,7 @@ set -ex
 
 sleep 30
 
+nohup unbound -c ${UNBOUND_CONFIGMAP_DIRECTORY}/unbound.conf &
 while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
 unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
 
