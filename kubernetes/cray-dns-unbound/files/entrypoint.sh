32744e76edce537245f4c80a7883cc9ddd89e911
Dat Le <dle@cray.com>
2021-12-13 09:32:13 -0600

    updating kubectl version

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-sleep 30
+sleep 10
 
 while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
 unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
