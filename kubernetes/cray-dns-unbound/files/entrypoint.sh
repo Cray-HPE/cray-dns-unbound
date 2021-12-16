bfa909f3371f8844930bacf824b989fba0b58080
Dat Le <dle@cray.com>
2021-12-16 13:44:10 -0600

    adjust entrypoint sleep

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # give time for pod to settle
-sleep 15
+sleep 5
 
 # initialize.py will start unbound if unbound is not already running
 while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done
