a5900cd61f28b1c59374cdc63a56b4416bd335d0
dle-hpe <dat.le@hpe.com>
2022-11-28 15:07:42 -0600

    update coredns.py to increase concurrent limit to unbound from coredns

--- kubernetes/cray-dns-unbound/files/coredns.py
+++ kubernetes/cray-dns-unbound/files/coredns.py
@@ -33,6 +33,7 @@ while True:
             raise SystemExit(err)
 
 corefile = re.sub(r'(?m)(^\s*)forward.*$', r'\1forward . %s {' % os.environ['NMN_LOAD_BALANCER_IP'], corefile)
+corefile = re.sub(r'(?m)(^\s*)max_concurrent.*$', r'\1max_concurrent %s' % 10000,corefile)
 
 corefile_patch = """
 data:
