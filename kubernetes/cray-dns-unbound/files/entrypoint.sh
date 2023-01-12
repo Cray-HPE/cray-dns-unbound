2487595ce0cf11700c5dc10072f003a7f2dd4756
Chris Spiller <86013738+spillerc-hpe@users.noreply.github.com>
2023-01-12 09:56:24 +0000

    CASMNET-2001 - Unbound metrics missing from Prometheus

--- kubernetes/cray-dns-unbound/files/entrypoint.sh
+++ kubernetes/cray-dns-unbound/files/entrypoint.sh
@@ -4,5 +4,4 @@ set -ex
 
 sleep 5
 
-while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done &
-unbound-telemetry tcp --bind --control-interface ${UNBOUND_CONTROL_INTERFACE}:${UNBOUND_PORT}
+while true; do /srv/unbound/initialize.py; sleep ${DNS_INITIALIZE_INTERVAL_SECONDS}; done 
