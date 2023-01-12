af47994a1acd67a1ed676fff5f8e846c1dfda602
Chris Spiller <86013738+spillerc-hpe@users.noreply.github.com>
2023-01-12 10:48:50 +0000

    Remove unused unbound-telemetry source and increment version

--- unbound-telemetry/fuzz/fuzz_targets/fuzz_measurement_observe.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-use unbound_telemetry::{Statistics, Measurement};
-
-fuzz_target!(|data: Statistics| {
-    let _ = Measurement::observe(data).unwrap();
-});
